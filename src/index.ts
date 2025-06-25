3a9ff1062e14a6608afcb3cedcbbf5624702199e
STY <kid101x@outlook.com>
2025-06-26 02:21:35 +0800

    fix /version

--- src/index.ts
+++ src/index.ts
@@ -386,8 +386,10 @@ export default {
 	fetch: async (request: Request, env: Env, ctx: ExecutionContext) => {
 		await new TelegramBot(env.SECRET_TELEGRAM_API_TOKEN)
 			.on('version', async (bot) => {
-				// <-- /version 命令处理器
-				const sha = env.CF_PAGES_COMMIT_SHA || 'unknown';
+				// /version 命令处理器
+				// 读取我们自己的 GIT_COMMIT_SHA 变量
+				const sha = env.GIT_COMMIT_SHA || 'unknown';
+				// 取前7位通常就足够唯一了
 				const versionMessage = `当前版本: \`${sha.slice(0, 7)}\``;
 				await bot.reply(versionMessage, 'MarkdownV2');
 				return new Response('ok');
